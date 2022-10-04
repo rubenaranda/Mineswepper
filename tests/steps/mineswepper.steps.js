624ba5f736e2f02197fcfee3d4069f0ca5bf69e5
rayor <ruben.aranda.7e5@itb.cat>
2022-10-04 12:14:49 +0200

    Scenario: Uncover a square without mine

--- tests/steps/mineswepper.steps.js
+++ tests/steps/mineswepper.steps.js
@@ -2,24 +2,36 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 
 const url = 'http://127.0.0.1:5500/src/html/mineswepper.html';
-const mockdata = "http://127.0.0.1:5500/src/html/"
 
+async function cellDiscover(string) {
+	await page.click(`[data-testid="${string}"]`, { force: true });
+}
+
+//Background scenario
 Given("a user opens the game",async function() {
     await page.goto(url);
 })
 
-
-// 1 Scenario: Uncover a square: -> Uncover a square without a mine
+// To load mockData tests
 Given('the following mockdata is loaded: {string}', async function (mockData) {
-    await page.goto(mockdata + '?mockData=' + mockData);
+    await page.goto(url + '?mockData=' + mockData);
   })
- When('the user un cover the square {string}',async function (string) {
-    // Write code here that turns the phrase above into concrete actions  
+
+  // To uncover a cell with the position passed by the feature file
+ When('the user uncover the square {string}',async function (string) {
+    await cellDiscover(string)
   });
 
+  // To check if the cell is uncoverd
+  Then('the square {string} should be uncovered',async function (string) {
+    const display = await page.locator('data-testid='+string).innerText();
+    expect(display).toBe("")
+  });
 
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString)
-  const mockData = urlParams.get('mockData')
+  
+  Then('the square {string} should show an explosion simbol', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
 
   
